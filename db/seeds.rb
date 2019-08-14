4a1fdbec5882fc2ef4ab4317bd43ddaa85c73c38
maxence ballande <maxence.ballande.16@neoma-bs.com>
2019-08-14 17:33:17 +0200

    resto works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number:  "+44 44 44 44 44"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number:  "+33 6 23 52 54 54"
+  },
+  {
+    name:         'Sushi Shop',
+    address:      'villa gaudelet',
+    category:     'japanese',
+    phone_number: '+33 1 23 52 54 54'
+  },
+  {
+    name:         'La Baguette',
+    address:      'villa marmelade',
+    category:     'japanese',
+    phone_number: '+33 3 12 34 56 78'
+  },
+  {
+    name:         'Fresher',
+    address:      'salad town',
+    category:     'french',
+    phone_number: '+33 4 45 43 54 35'
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
